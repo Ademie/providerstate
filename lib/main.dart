e4304e94ce14aef86dba147d607314446bc57680
ademie <isijolaademie@gmail.com>
2023-03-31 07:16:36 -0600

    managed state using stateful widget alone

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,49 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<int> numbers = [1, 2, 3, 4];
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            int last = numbers.last;
+            setState(() {
+              numbers.add(last + 1);
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              Text(
+                numbers.last.toString(),
+                style: TextStyle(fontSize: 30),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: numbers.length,
+                  itemBuilder: (context, index) {
+                    return Text(
+                      numbers[index].toString(),
+                      style: TextStyle(fontSize: 30),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
